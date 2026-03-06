f183ab19003a1d602b32b56a2cc624100296f94a
ishika rawat <ishika.sabai@gmail.com>
2026-03-06 21:19:31 +0530

    latest_commit

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(
 
 // ✅ SECURITY HEADERS (Fix for COOP Issue)
 app.use((req, res, next) => {
-    res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
     res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
     next();
 });
