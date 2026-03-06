5a60ee437d3a2f9f4cdd0218c46e12abcc35b12e
ishika rawat <ishika.sabai@gmail.com>
2026-03-06 21:14:45 +0530

    latest_commit

--- you_tube/next.config.ts
+++ you_tube/next.config.ts
@@ -6,7 +6,20 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
     BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
